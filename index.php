4e09ea8e00b468cb698675edc1e2a3953659ea7e
Piyush Mishra <Piyushkrm03@gmail.com>
2024-01-03 20:11:36 +0530

    Upload About Folder

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<link rel="shortcut icon" type="image/x-icon" href="../img/logoo.png" />
+	<title>About Us</title>
+</head>
+
+<body>
+	<header>
+		<nav>
+			<div class="logo">Test 4 You</div>
+			<ul class="nav-links">
+				<li><a href="http://localhost/Project-K/Home/index.php">Home</a></li>
+				<li><a href="http://localhost/Project-K/Home/About/index.php">About</a></li>
+				<li><a href="http://localhost/Project-K/Home/Registration/index.php">Login</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<section class="about">
+		<h1>About Us</h1>
+		<p style="font-weight: bold">Mock Test Skill Development...</p>
+		<div class="about-info">
+			<div class="about-img">
+				<img src="../img/logoo.png" alt="Mock Test 4 You">
+			</div>
+			<div>
+	            <p>The project: “Mock-Test” is a collection of number of different types of tests like Banking and Insurance, SSC/Railways, Civil Services, Agriculture Exams, Regulatory, MBA, Defence Exams, Laws Exams, etc. A user can access all of the tests and can attempt any of the one. There will be limited number of questions and for each correct answer user will get a one credit score. User can see answers as well as can ask a query related to it. There are many Mock-Test applications available currently on internet. But there are few Which provide better understanding between users and the application like, providing proper answers, user query solving, etc. To develop a user-friendly Mock-Test web application which will contain : Numbers of tests , Answers to every question, and to improve the knowledge level of users. To develop a web application which will contain solution to the above problems. By this web application the user will come to know about his/her level and can learn additional knowledge. Also, by this web application a user can expand his/her knowledge among the world.</p>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
